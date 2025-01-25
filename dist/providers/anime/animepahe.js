cb384ecf8e7b97fcd4271b79c2e02858418ac137
MemeSlayer9 <70009317+MemeSlayer9@users.noreply.github.com>
2025-01-26 00:17:28 +0800

    Update animepahe.js

--- dist/providers/anime/animepahe.js
+++ dist/providers/anime/animepahe.js
@@ -7,7 +7,7 @@ class AnimePahe extends models_1.AnimeParser {
     constructor() {
         super(...arguments);
         this.name = 'AnimePahe';
-        this.baseUrl = 'https://animepahe.com';
+        this.baseUrl = 'https://animepahe.ru';
         this.logo = 'https://animepahe.com/pikacon.ico';
         this.classPath = 'ANIME.AnimePahe';
         // private readonly sgProxy = 'https://cors.consumet.stream';
